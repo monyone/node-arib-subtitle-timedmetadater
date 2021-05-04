087cb5f76c67aa0f4ba336bbc424f4e9f407570a
monyone <monyone.teihen@gmail.com>
2021-05-04 21:05:40 +0900

    UDP 送信に対応してみた (いらなかったら Revert する)

--- src/arib-subtitle-timedmetadater.ts
+++ src/arib-subtitle-timedmetadater.ts
@@ -15,12 +15,16 @@ program
   .option('-t, --tcp_port <port>', 'input tcp port')
   .option('-u, --udp_port <port>', 'input udp port')
   .option('-o, --output <path>', 'output mpeg2ts path')
+  .option('-h, --output_host <host>', 'output mpeg2ts UDP host')
+  .option('-p, --output_port <port>', 'output mpeg2ts UDP port')
+  .option('-c, --packet_count <size>', 'output mpeg2ts UDP packet buffer size')
+
 program.parse(process.argv);
 const options = program.opts();
 
 // ffmpeg が UDP 受けで descriptor を保持するようになったらUDP対応を外す (これは ffmpeg 4.5 で確定なはず)
 const src = new PassThrough(); 
-const dst = options.output == null || options.output === '-' ? process.stdout : fs.createWriteStream(options.output);
+const dst = new PassThrough();
 
 src
   .pipe(new MetadataTransform())
@@ -40,4 +44,24 @@ if (options.tcp_port) {
 } else {
   const stream = options.input == null || options.input === '-' ? process.stdin : fs.createReadStream(options.input);
   stream.pipe(src);
+}
+
+if (options.output_port) {
+  const socket = dgram.createSocket('udp4')
+  const host = options.output_host ?? 'localhost';
+  const port = options.output_port ?? 8000
+  const count = options.packet_count ?? 100 // MTU 1500 byte だから 7 くらいが限度な気がするけど、ローカルホストだと 100 くらいで安定した
+
+  let block: Buffer = Buffer.from([]);
+
+  dst.on('data', (chunk: Buffer) => {
+    block = Buffer.concat([block, chunk]);
+    if (block.length >= 188 * count) {
+      socket.send(block, 0, block.length, port, host);
+      block = Buffer.from([]);
+    }
+  });
+} else {
+  const output = options.output == null || options.output === '-' ? process.stdout : fs.createWriteStream(options.output);
+  dst.pipe(output)
 }
