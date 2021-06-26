e2a7289f9ad5bb7441f8f0db71b27dc552f849eb
monyone <monyone.teihen@gmail.com>
2021-06-26 17:19:36 +0900

    bind するアドレスを明示的に指定できるようにして、デフォルトは 0.0.0.0 ではなく localhost とするように変更

--- src/arib-subtitle-timedmetadater.ts
+++ src/arib-subtitle-timedmetadater.ts
@@ -14,18 +14,16 @@ program
   .option('-i, --input <path>', 'input mpeg2ts path')
   .option('-t, --tcp_port <port>', 'input tcp port')
   .option('-u, --udp_port <port>', 'input udp port')
-  .option('-o, --output <path>', 'output mpeg2ts path')
-  .option('-h, --output_host <host>', 'output mpeg2ts UDP host')
-  .option('-p, --output_port <port>', 'output mpeg2ts UDP port')
-  .option('-c, --packet_count <size>', 'output mpeg2ts UDP packet buffer size')
-  .option('-b, --buffer_size <size>', 'UDP buffer size')
+  .option('-h, --host <address>', 'input host address')
+  .option('-b, --recv_buffer_size <size>', 'input udp recvBufferSize')
+  .option('-o, --output <path>', 'output mpeg2ts path');
 
 program.parse(process.argv);
 const options = program.opts();
 
 // ffmpeg が UDP 受けで descriptor を保持するようになったらUDP対応を外す (これは ffmpeg 4.5 で確定なはず)
 const src = new PassThrough(); 
-const dst = new PassThrough();
+const dst = options.output == null || options.output === '-' ? process.stdout : fs.createWriteStream(options.output);
 
 src
   .pipe(new MetadataTransform())
@@ -35,40 +33,20 @@ if (options.tcp_port) {
   const server = net.createServer((socket) => {
     socket.pipe(src);
   });
-  server.listen(options.tcp_port);
+  server.listen({
+    port: options.tcp_port,
+    host: options.host ?? 'localhost'
+  });
 } else if (options.udp_port) {
   const server = dgram.createSocket({
     type: 'udp4',
-    recvBufferSize: options.buffer_size ?? 1000000,
+    recvBufferSize: options.recv_buffer_size ?? 1000000,
   })
   server.on('message', (msg: Buffer) => {
     src.write(msg);
   })
-  server.bind(options.udp_port);
+  server.bind(options.udp_port, options.host ?? 'localhost');
 } else {
   const stream = options.input == null || options.input === '-' ? process.stdin : fs.createReadStream(options.input);
   stream.pipe(src);
-}
-
-if (options.output_port) {
-  const socket = dgram.createSocket({
-    type: 'udp4',
-    sendBufferSize: options.buffer_size ?? 1000000,
-  })
-  const host = options.output_host ?? 'localhost';
-  const port = options.output_port ?? 8000
-  const count = options.packet_count ?? 100 // MTU 1500 byte だから 7 くらいが限度な気がするけど、ローカルホストだと 100 くらいで安定した
-
-  let block: Buffer = Buffer.from([]);
-
-  dst.on('data', (chunk: Buffer) => {
-    block = Buffer.concat([block, chunk]);
-    if (block.length >= 188 * count) {
-      socket.send(block, 0, block.length, port, host);
-      block = Buffer.from([]);
-    }
-  });
-} else {
-  const output = options.output == null || options.output === '-' ? process.stdout : fs.createWriteStream(options.output);
-  dst.pipe(output)
 }
